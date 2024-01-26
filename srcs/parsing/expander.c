26fca4b84281c706e9f065a093b0b79d6e37107d
Juliette Barbay <barbayjuliette95@gmail.com>
2024-01-26 16:11:05 +0800

    expand environment variables

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -6,14 +6,14 @@
 /*   By: jbarbay <jbarbay@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:54:06 by jbarbay           #+#    #+#             */
-/*   Updated: 2024/01/26 14:56:38 by jbarbay          ###   ########.fr       */
+/*   Updated: 2024/01/26 16:07:24 by jbarbay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 // Environment variable cannot start with a digit
-// Only uppercase, lowercase, and underscores are allowed for environment variables
+// Only uppercase, lowercase, digits and underscores are allowed for environment variables
 // Not used here anymore. We only need to check if variable is valid during export, not when using it.
 // I leave it so you can use it at execution.
 
@@ -30,36 +30,33 @@ int	get_variable_len(char *str)
 
 	i = 0;
 	while (str[i] && str[i] != ' ' && var_is_valid(str[i], i))
+	{
+		if (i == 0 && ft_isdigit(str[i]))
+			return (0);
 		i++;
+	}
 	return (i);
 }
 
-// char	*get_ext_status(char *str, int *i, int len)
-// {
-
-// }
-
-char	*get_variable(char *str, int *i, int len, int exit_status)
+char	*get_value_variable(char *str, int i, int len)
 {
 	char	*var;
 	char	*value;
+
+	var = ft_substr(str + i + 1, 0, len);
+	value = getenv(var);
+	if (!value)
+		value = "";
+	free(var);
+	return (value);
+}
+
+char	*update_string(char *str, int *i, int len, char *value)
+{
 	char	*prefix;
 	char	*suffix;
 	char	*join;
 
-	if (str[*i + 1] && str[*i + 1] == '?')
-	{
-		len = 1;
-		value = ft_itoa(exit_status); // Malloced string
-	}
-	else
-	{
-		var = ft_substr(str + *i + 1, 0, len);
-		value = getenv(var);
-		if (!value)
-			value = "";
-		free(var);
-	}
 	prefix = ft_substr(str, 0, *i);
 	suffix = ft_substr(str, len + *i + 1, ft_strlen(str) - len - *i - 1);
 	join = ft_strjoin(prefix, value);
@@ -72,8 +69,33 @@ char	*get_variable(char *str, int *i, int len, int exit_status)
 	return (str);
 }
 
-// TEST: thisis$USER"xest"$USER
+char	*get_variable(char *str, int *i, int len, int exit_status)
+{
+	char	*value;
+	char	*val;
+	int		malloced; // Tells us if value was malloced and will need to be freed
 
+	malloced = 0;
+	if (str[*i + 1] && str[*i + 1] == '?')
+	{
+		len = 1;
+		value = ft_itoa(exit_status); // Malloced string
+		malloced = 1;
+	}
+	else if (len == 0)
+	{
+		value = "";
+		len = 1;
+	}
+	else
+		value = get_value_variable(str, *i, len);
+	str = update_string(str, i, len, value);
+	if (malloced)
+		free(value);
+	return (str);
+}
+
+// TEST: thisis$USER"xest"$USER
 // Replace 127 with exit status
 
 char	*get_var_double(char *str, int *i)
@@ -83,7 +105,7 @@ char	*get_var_double(char *str, int *i)
 	(*i)++;
 	while (str[*i] && str[*i] != 34)
 	{
-		if (str[*i] == '$')
+		if (str[*i] == '$' && str[*i + 1] && str[*i + 1] != ' ' && str[*i + 1] != 34)
 		{
 			len = get_variable_len(str + *i + 1);
 			str = get_variable(str, i, len, 127);
@@ -109,7 +131,7 @@ void	expand_variables(char **value)
 			i += find_index(str + i + 1, 39);
 		else if (str[i] == 34)
 			str = get_var_double(str, &i);
-		else if (str[i] == '$')
+		else if (str[i] == '$' && str[i + 1] && str[i + 1] != ' ')
 		{
 			len = get_variable_len(str + i + 1);
 			str = get_variable(str, &i, len, 127);
@@ -127,3 +149,21 @@ void	expand_all(t_token *token)
 		token = token->next;
 	}
 }
+
+// Invalid first character of environment vraiable, or variable not found -> replace with empty string
+
+// There are many different ways bash handles $ when the characters following $ are invalid.
+// So I simplified it.
+
+// echo $,TEST --> $,TEST
+// echo $*TEST --> TEST
+// echo $9TEST --> TEST
+
+// I handled it as :
+// If the first character after the $ is invalid, I replace the $9 with an empty string, like echo $9TEST --> TEST
+// To get the variable name. I get the characters, as long as they are valid. Once the char is invalid, I stop adding it as the variable name.
+// echo $USER%TEST--> Varibale name is USER.
+// Bash : echo $USER% --> juliettebarbay%
+
+// If I have "This costs 45$" or "This costs $ 45"
+// Output is the same, $ is not a variable there
